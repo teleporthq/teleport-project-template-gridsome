5ea4dd41cdc682a79e060bab9f6ef9adc5cc0bbd
JayaKrishna <namburu1995@gmail.com>
2019-10-17 11:59:09 +0530

    Initial template setup

---
+++ src/main.js
@@ -0,0 +1,4 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+export default function(Vue, { router, head, isClient }) {}
